18343c4ebe8af0efa5bd1576831bc10dece478b1
mehdi <0r0.developer@gmail.com>
2022-01-08 22:25:10 +0400

    using queueArray

--- src/main/java/com/algo/App.java
+++ src/main/java/com/algo/App.java
@@ -2,13 +2,7 @@ package com.algo;
 
 
 
-import com.algo.BalancedStringExpression.BalancedExpression;
-import com.algo.QueueExample.QueueAlgorithms;
-import com.algo.TreeExample.Tree;
-
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Queue;
+import com.algo.QueueExample.ArrayQueue;
 
 /**
  * Hello world!
@@ -16,15 +10,36 @@ import java.util.Queue;
 public class App {
         public static void main(String[] args) {
 
-                Queue<Integer> queue = new ArrayDeque<Integer>();
-                queue.add(10);
-                queue.add(20);
-                queue.add(30);
-                queue.add(40);
-                queue.add(50);
+            ArrayQueue arrayQueue=new ArrayQueue(8);
 
-               QueueAlgorithms.reverse1(queue);
-                QueueAlgorithms.reverse2(queue);
+            arrayQueue.enqueue(100);
+            arrayQueue.enqueue(200);
+            arrayQueue.enqueue(300);
+            arrayQueue.enqueue(400);
+            System.out.println(arrayQueue.dequeue());
+            System.out.println(arrayQueue.dequeue());
+            System.out.println(arrayQueue.dequeue());
+            System.out.println(arrayQueue.dequeue());
+            System.out.println(arrayQueue.dequeue());
+            System.out.println(arrayQueue.dequeue());
+            System.out.println(arrayQueue);
+            arrayQueue.enqueue(101);
+            arrayQueue.enqueue(201);
+            arrayQueue.enqueue(301);
+            arrayQueue.enqueue(401);
+            arrayQueue.enqueue(501);
+            arrayQueue.enqueue(601);
+            System.out.println(arrayQueue);
+            System.out.println(arrayQueue.dequeue());
+            System.out.println(arrayQueue.dequeue());
+            System.out.println(arrayQueue.dequeue());
+            System.out.println(arrayQueue.dequeue());
+            System.out.println(arrayQueue.dequeue());
+            System.out.println(arrayQueue);
+            System.out.println(arrayQueue.dequeue());
+            System.out.println(arrayQueue.dequeue());
+            System.out.println(arrayQueue);
+
         }
 
 
