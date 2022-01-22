75b019e69895727ca5ed1e98d17e3c6e7688cf73
mehdi <0r0.developer@gmail.com>
2022-01-23 01:34:51 +0400

    remove unused modules

--- src/main/java/com/algo/App.java
+++ src/main/java/com/algo/App.java
@@ -1,29 +1,17 @@
 package com.algo;
 
 
-
-import com.algo.HashTables.NonRepeatedCharacter;
-import com.algo.QueueExample.ArrayQueue;
-import com.algo.QueueExample.QueueByStack;
-import com.algo.QueueExample.QueueByTwoStacks;
 import com.algo.sets.FirstRepeatedCharacter;
 
-/**
- * Hello world!
- */
 public class App {
-        public static void main(String[] args) {
+    public static void main(String[] args) {
 
 
-            FirstRepeatedCharacter find=new FirstRepeatedCharacter("absdcefghia");//must return a
-            System.out.println(find.findFirstRepeatedCharacter());
+        FirstRepeatedCharacter find = new FirstRepeatedCharacter("absdcefghia");//must return a
+        System.out.println(find.findFirstRepeatedCharacter());
 
 
-
-
-
-        }
-
+    }
 
 
 }
