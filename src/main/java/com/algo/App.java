e968e78f3ff8d82585ed078ee60b94eb79deb1cf
mehdi <0r0.developer@gmail.com>
2022-02-04 00:21:22 +0400

    .

--- src/main/java/com/algo/App.java
+++ src/main/java/com/algo/App.java
@@ -2,25 +2,16 @@ package com.algo;
 
 
 import com.algo.HashTables.HashTableImplementation;
+import com.algo.TreeExample.Tree;
 
 
 public class App {
     public static void main(String[] args) {
-
-
-
-        HashTableImplementation myHash=new HashTableImplementation(5);
-        myHash.put(6,"one");
-        myHash.put(8,"two");
-        myHash.put(11,"ht");
-        myHash.put(6,"one+++");
-        System.out.println("=================");
-        System.out.println(myHash.get(6));
-        System.out.println(myHash.get(8));
-        System.out.println(myHash.get(11));
-        System.out.println("Done");
-
-
+        Tree tree=new Tree();
+        tree.insert(6);
+        tree.insert(4);
+        tree.insert(11);
+        System.out.println("done");
 
     }
 
