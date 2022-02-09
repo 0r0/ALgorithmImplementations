6280fc3e3fc7fc286b4620d6faf29d2210986910
mehdi <0r0.developer@gmail.com>
2022-02-09 15:37:41 +0400

    implementation of insert heap

--- src/main/java/com/algo/App.java
+++ src/main/java/com/algo/App.java
@@ -3,21 +3,19 @@ package com.algo;
 
 import com.algo.AVLTrees.AVLTree;
 import com.algo.HashTables.HashTableImplementation;
+import com.algo.Heap.Heaps;
 import com.algo.TreeExample.Tree;
 
 
 public class App {
     public static void main(String[] args) {
-        var tree=new AVLTree();
-        tree.insert(6);
-        tree.insert(4);
-        tree.insert(11);
-        tree.insert(7);
-        tree.insert(8);
-//        tree.insert(9);
-//        tree.insert(21);
-//        tree.insert(2);
-         System.out.println(tree);
+        var heaps=new Heaps();
+        heaps.insert(10);
+        heaps.insert(20);
+        heaps.insert(30);
+        heaps.insert(7);
+        heaps.insert(11);
+         System.out.println("done");
 
 
 
--- src/main/java/com/algo/Heap/Heaps.java
+++ src/main/java/com/algo/Heap/Heaps.java
@@ -1,8 +1,39 @@
 package com.algo.Heap;
 
 public class Heaps {
+    private int[] heap=new int[10];
+    private int size;
 
-    public void insert(){
+
+    public void insert(int value){
+        if(isFull())
+            throw  new IllegalStateException();
+        heap[size++]=value;
+        bubbleUp();
 
     }
+    private boolean isFull(){
+        return size==heap.length;
+    }
+    private void bubbleUp(){
+        var index=size-1;
+        while(index>0 && heap[index]>heap[parent(index)])
+        {
+            swap(index,parent(index));
+            index=parent(index);
+        }
+    }
+    private int parent(int index)
+    {
+        return (index-1)/2;
+    }
+
+    private void swap(int first,int second)
+    {
+        var temp=heap[first];
+        heap[first]=heap[second];
+        heap[second]=temp;
+    }
+
+
 }
