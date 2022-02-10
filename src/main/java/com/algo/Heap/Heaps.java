dedbf5dc9a3ae079bda6fcd1a7e5280adab09832
mehdi <0r0.developer@gmail.com>
2022-02-10 20:35:03 +0400

    implementation of remove from  heap section 1

--- src/main/java/com/algo/Heap/Heaps.java
+++ src/main/java/com/algo/Heap/Heaps.java
@@ -35,5 +35,47 @@ public class Heaps {
         heap[second]=temp;
     }
 
+    public void remove()
+    {
+        if(isEmpty())
+            throw new IllegalStateException();
+        var index=0;
+        heap[index]=heap[--size];
+        while(index<size && !isValid(index))
+        {
+            var largerChildIndex=largerChildIndex(index);
+            swap(index,largerChildIndex);
+            index=largerChildIndex;
+        }
+
+    }
+    private boolean isEmpty()
+    {
+        return size==0;
+    }
+    private int largerChildIndex(int index)
+    {
+        return leftChild(index)>rightChild(index) ?leftChildIndex(index):rightChildIndex(index);
+    }
+    private boolean isValid(int index){
+        return heap[index]>leftChild(index) && heap[index]>rightChild(index) ;
+    }
+    private int leftChild(int index)
+    {
+        return heap[leftChildIndex(index)];
+    }
+    private int rightChild(int index)
+    {
+        return heap[rightChildIndex(index)];
+    }
+    private int  leftChildIndex(int index)
+    {
+        return index*2+1;
+
+    }
+    private int rightChildIndex(int index)
+    {
+        return index*2+2;
+    }
 
 }
